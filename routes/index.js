1dd5164b051e288807af8e31257f2fea269ef24b
Kyle Mattimore <kmattimo@umich.edu>
2017-02-16 10:11:45 -0600

    check api

--- routes/index.js
+++ routes/index.js
@@ -37,6 +37,8 @@ exports = module.exports = function (app) {
 	app.get('/', routes.views.index);
 	app.all('/contact', routes.views.contact);
 
+	app.get('/check', routes.api.check);
+
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
 
