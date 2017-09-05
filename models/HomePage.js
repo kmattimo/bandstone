5ba0bdb2e9e27173fda1cd00452c363131b91243
Kyle Mattimore <kmattimo@umich.edu>
2017-09-04 22:21:42 -0500

    remove hardcoded text

--- models/HomePage.js
+++ models/HomePage.js
@@ -4,10 +4,11 @@ var Types = keystone.Field.Types;
 var HomePage = new keystone.List('HomePage');
 
 HomePage.add({
-	eventLabel: { type: Types.Html, wysiwyg: true, index: true },
-	events: { type: Types.Html, initial: true, wysiwyg: true, index: true },
+	// eventLabel: { type: Types.Html, wysiwyg: true},
+	// events: { type: Types.Html, initial: true, wysiwyg: true, },
+
 	// analytics: {type: Types.Html },
-	elevatorpitch: { type: Types.Html},
+	elevatorpitch: { type: Types.Html, wysiwyg: true},
 	soundcloud: { type: Types.Html },
 	youtube: { type: Types.Html },
 });
